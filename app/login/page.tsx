8a3a825ba6ad4f2d317f644e4010fea14ccb39a4
lukemitchelladams <lukemitchelladams@gmail.com>
2026-04-07 16:20:14 -0600

    Add delay before redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
       setError(error.message)
       setLoading(false)
     } else {
-      window.location.href = '/dashboard'
+      setTimeout(() => { window.location.href = '/dashboard' }, 1000)
     }
   }
 
